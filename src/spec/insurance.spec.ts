e1bca001e69881742766706d4280d00077b6902b
Cristopher Herrera <c.herreratapia@hotmail.com>
2019-12-08 11:38:51 -0300

    Error in test. No exists classes for Rule and Effect

--- src/spec/insurance.spec.ts
+++ src/spec/insurance.spec.ts
@@ -4,11 +4,65 @@ import CarInsurance from "../models/CarInsurance";
 describe("CarInsurance object", () => {
   it("Should create an Array of objects. Then create an insurance Product", () => {
     const products = [
-      new Product("Product 1", 10, 10),
-      new Product("Product 2", 10, 10),
-      new Product("Product 3", 4, 4)
+      new Product("Medium Coverage", 10, 10),
+      new Product("Full Coverage", 10, 10),
+      new Product("Mega Coverage", 1, 180),
+      new Product("Special Full Coverage", 15, 10),
+      new Product("Super Sale", 15, 10)
     ];
-    const carInsurance = new CarInsurance(products);
+
+    const effectSellInMinusOne = new Effect("sellIn", "-", "1");
+    const effectPriceMinusOne = new Effect("Price", "-", "1");
+    const effectPriceAddOne = new Effect("Price", "+", "1");
+    const effectPriceAddTwo = new Effect("Price", "+", "2");
+    const effectPriceAddThree = new Effect("Price", "+", "3");
+    const effectPriceMinusTwo = new Effect("Price", "-", "2");
+    const effectPriceToZero = new Effect("Price", "=", 0);
+
+    const rules: Rule[] = [
+      new Rule("Medium Coverage", "daily", "sellIn", 0, effectSellInMinusOne),
+      new Rule("Medium Coverage", "daily", "sellIn", 0, effectPriceMinusOne),
+      new Rule("Full Coverage", "daily", "sellIn", 0, effectSellInMinusOne),
+      new Rule("Full Coverage", "daily", "sellIn", 0, effectPriceAddOne),
+      new Rule(
+        "Special Full Coverage",
+        "greaterThan",
+        "sellIn",
+        10,
+        effectPriceMinusOne
+      ),
+      new Rule(
+        "Special Full Coverage",
+        "daily",
+        "sellIn",
+        0,
+        effectSellInMinusOne
+      ),
+      new Rule(
+        "Special Full Coverage",
+        "lessThanOrEqual",
+        "sellIn",
+        10,
+        effectPriceAddTwo
+      ),
+      new Rule(
+        "Special Full Coverage",
+        "lessThanOrEqual",
+        "sellIn",
+        5,
+        effectPriceAddThree
+      ),
+      new Rule(
+        "Special Full Coverage",
+        "equal",
+        "sellIn",
+        0,
+        effectPriceToZero
+      ),
+      new Rule("Super Sale", "daily", "sellIn", 0, effectSellInMinusOne),
+      new Rule("Super Sale", "daily", "sellIn", 0, effectPriceMinusTwo)
+    ];
+    const carInsurance = new CarInsurance(products, rules);
     expect(carInsurance.product.length).toBe(3);
   });
 });
